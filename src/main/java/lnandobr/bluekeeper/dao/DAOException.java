dd0de15b09e4940640dd8724dfe51b4fec4cece3
ffernandoadriano <lfernando.adriano@gmail.com>
2024-04-10 11:20:48 -0300

    Exceção de DAO

---
+++ src/main/java/lnandobr/bluekeeper/dao/DAOException.java
@@ -0,0 +1,26 @@
+package lnandobr.bluekeeper.dao;
+
+/**
+ * Exceção de DAO
+ */
+public class DAOException extends RuntimeException {
+
+    public DAOException() {
+    }
+
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DAOException(Throwable cause) {
+        super(cause);
+    }
+
+    public DAOException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
