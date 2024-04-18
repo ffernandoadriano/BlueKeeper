b363168c831e37f9ccfb660ad79316a021aabadd
ffernandoadriano <lfernando.adriano@gmail.com>
2024-04-18 09:09:25 -0300

    Classe principal

---
+++ src/main/java/lnandobr/bluekeeper/application/Main.java
@@ -0,0 +1,10 @@
+package lnandobr.bluekeeper.application;
+
+/**
+ * Classe principal
+ */
+public class Main {
+    public static void main(String[] args) {
+        Reference.main(args);
+    }
+}
